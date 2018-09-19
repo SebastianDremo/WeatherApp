5150b3be1fd6e15eefabe2141db3bc7c48a3cc16
Sebastian Dremo <sebastiandremo@gmail.com>
2018-09-19 21:07:35 +0200

    Created model wich handle consuming API

---
+++ WeatherApp/WeatherApp/Controllers/WeatherApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+/*
+    * http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+    *  APIKEY = 9a5a6f66668c6b8861c56d58d5b5d4e3
+    * */
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherApiController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Weather()
+        {
+                        
+
+            return View();
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/Models/ApiClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Models
+{
+    public class ApiClient
+    {
+        public async Task<WeatherApiModel> GetCity(string cityName)
+        {
+            using(var client = new HttpClient())
+            {
+                
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"data/2.5/weather?q={cityName}&apiid=9a5a6f66668c6b8861c56d58d5b5d4e3");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult =await response.Content.ReadAsStringAsync();
+                    var weather = JsonConvert.DeserializeObject<WeatherApiModel>(stringResult);
+
+                    return weather;              
+            }
+        }
+        
+    }
+}
