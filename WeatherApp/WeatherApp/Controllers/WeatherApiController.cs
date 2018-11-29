0ff8bc98490a2f694f325a2f26baf369543b4b09
sebastiandremo@gmail.com <sebastiandremo@gmail.com>
2018-11-29 20:22:11 +0100

    błąd

--- WeatherApp/WeatherApp/Controllers/WeatherApiController.cs
+++ WeatherApp/WeatherApp/Controllers/WeatherApiController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WeatherApp.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WeatherApp.Controllers
 {
     public class WeatherApiController : Controller
